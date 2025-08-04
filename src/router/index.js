008a679b156aae6d43a06fe0c70fdcb6bcc2ea48
markusohlsson <makkeytv@gmail.com>
2025-08-04 11:44:20 +0200

    Routing to pages updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../pages/Home.vue';
+import Projects from '../pages/Projects.vue';
+import Contact from '../pages/Contact.vue';
+import About from '../pages/About.vue';
 
 const routes = [
     { path: '/', component: Home, name: 'Home'},
+    { path: '/projects', component: Projects, name: 'Projects'},
+    { path: '/Contact', component: Contact, name: 'Contact'},
+    { path: '/About', component: About, name: 'About'},
 ];
 
 const router = createRouter({
