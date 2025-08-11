1e51ed08ebf804dc5a0d6cf40dfe9d6bfec22449
markusohlsson <makkeytv@gmail.com>
2025-08-11 12:17:47 +0200

    Removed under construction image and added css animation instead

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="projectcard" data-aos="fade-up">
-        <img class="projectcard-image" data-aos="zoom-in" src="../assets/svg/under_construction.svg" alt="">
+        <div class="projectcard-image"><Cog /></div>
         <h4 class="projectcard-title" data-aos="fade-up">{{title}}</h4>
         <p class="projectcard-description" data-aos="fade-up">{{description}}</p>
     </div>
 </template>
 
 <script setup>
+import { Cog } from 'lucide-vue-next';
     const props = defineProps({
         title: String,
         image: String,
         description: String,
     })
 </script>
+
