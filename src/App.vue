2a6059ead51d1e8b517d1018a9ceaab348a410a2
markusohlsson <makkeytv@gmail.com>
2025-08-18 10:45:00 +0200

    Added localstorage override for reduced motion setting

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,28 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useSettingsStore } from './stores/settings';
 
-const settings = useSettingsStore();
-settings.init();
+const settings = useSettingsStore()
+settings.init()
+
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches
+const lsSettings = JSON.parse(localStorage.getItem("settings") || "{}")
+
 onMounted(() => {
-  if (prefersReducedMotion) {
-    settings.reducedMotion = true
+  if (typeof lsSettings.reducedMotion === "boolean") {
+    settings.reducedMotion = lsSettings.reducedMotion
   } else {
-    settings.reducedMotion = false
+    settings.reducedMotion = prefersReducedMotion
   }
+
   if (!settings.reducedMotion) {
     AOS.init({
       duration: 800,
       offset: 90,
       once: false,
       mirror: true,
-    });
+    })
   }
-
-});
-
-
+})
 
 
 </script>
